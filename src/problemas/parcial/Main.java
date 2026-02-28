759ab8bb5ef12c52d0615eeb808ca3a494eace3d
wcordova <wcordova@beesion.com>
2026-02-28 09:33:38 -0600

    segundo problema

---
+++ src/problemas/parcial/Main.java
@@ -0,0 +1,17 @@
+package problemas.parcial;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hola mundo!");
+		numeroFactorial();
+
+	}
+	
+	// Problema 2
+	public static void numeroFactorial() {
+		//TODO logica de programacion
+	}
+
+}
